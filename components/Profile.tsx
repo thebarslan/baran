1c4d419cc29de5811b798855a4ce286dc1e6745f
Baran Arslan <barslanforbusiness@gmail.com>
2025-09-25 16:56:38 +0300

    Project text's font sizes have been changed

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -6,18 +6,18 @@ import { FaXTwitter, FaLinkedinIn } from "react-icons/fa6";
 import Link from "next/link";
 const Profile = () => {
    return (
-      <div className="profile flex gap-8 h-[128px]">
-         <div className="profile-image-container h-32 w-32 shrink-0">
+      <div className="profile flex lg:gap-8 gap-6 h-[128px]">
+         <div className="profile-image-container lg:h-32 lg:w-32 w-24 h-32 shrink-0 lg:mt-0 mt-2">
             <Image
                src={ProfieImage}
                alt="Profile"
                width={128}
                height={128}
-               className="rounded-full h-32 w-32 shadow-2xl object-cover shrink-0"
+               className="rounded-full lg:h-32 lg:w-32 w-24 h-24 shadow-lg object-cover shrink-0"
             />
          </div>
          <div className="profile-info-container flex flex-col gap-1 justify-center">
-            <h1 className="text-2xl font-bold">Baran Arslan</h1>
+            <h1 className="lg:text-2xl text-xl font-bold">Baran Arslan</h1>
             <p className="text-sm text-secondary-foreground">
                Full Stack & Mobile Developer
             </p>
